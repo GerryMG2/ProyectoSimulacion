b9bd0454ebab6509bceffd723d45c60cd66cbf5a
GerryMG2 <00032515@uca.edu.sv>
2021-07-04 21:22:00 -0600

    puta al fin

--- problemType/desp3d.bat
+++ problemType/desp3d.bat
@@ -1,12 +1,9 @@
 
-
-
-echo %3\ > eco.txt
 del %2\%1.boh
 del %2\%1.post.res
 del %2\%1.post.dat
 del %2\%1.err
 
 
-call Rscript.exe '%3'\desp3d.R '%2\%1'
-
+call  Rscript.exe %3\desp3d.R "%2\%1"
+echo %3\ > eco.txt
